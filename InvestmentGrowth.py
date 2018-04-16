25a38657cce360c8f9e765eee6d1daa4b2a73b45
Joshua Humphrey <JCHumphrey@drbsystems.com>
2018-04-16 13:46:11 -0400

    Added in historical bond data and created a UI indicator for different allocation strategies

--- InvestmentGrowth.py
+++ InvestmentGrowth.py
@@ -8,10 +8,13 @@ import random
 fields = ('Annual Contribution', 'Current Age', 'Retirement Age','Current Portfolio Value')
 ageSpread = [15]
 portfolioSpread = [0]
+strategies = {'Fixed Allocations', 'Shifting Bond Allocations'}
 
-with open('HistoricalReturns.txt') as file:
-    stockData = file.readlines()
+with open('stockHistory.txt') as stockFile:
+    stockData = stockFile.readlines()
 
+with open('bondHistory.txt') as bondFile:
+    bondData = bondFile.readlines()
 
 def makeform(root, fields):
    entries = {}
@@ -36,7 +39,7 @@ def CalculatePortfolio(entries):
     balance = float(portfolioSpread[0])
 
     while (currentAge < retirementAge+1):
-        randomReturnRate = FindRandomReturnRate()
+        randomReturnRate = FindRandomStockRate()
         balance = balance + contribution
         balance = balance + balance*randomReturnRate
         currentAge = currentAge + 1
@@ -49,9 +52,13 @@ def CalculatePortfolio(entries):
             finalBalance.pack(side=BOTTOM)
     PlotChart(ageSpread,portfolioSpread)
 
-def FindRandomReturnRate():
+def FindRandomStockRate():
     randomYear = random.randint(1,88)
     return float(stockData[randomYear])
+
+def FindRandomBondRate():
+    randomYear = random.randint(1,30)
+    return float(bondData[randomYear])
 
 def PlotChart(ageSpread,portfolioSpread):
     try:
@@ -74,12 +81,22 @@ def CreateInitialFigure():
     plot_widget = canvas.get_tk_widget()
     plot_widget.pack()
 
+def CreateAllocationPopup():
+    tkvar = StringVar(root)
+    tkvar.set('Shifting Bond Allocation')
+    popupMenu = OptionMenu(root, tkvar, *strategies)
+    Label(root, text='Portfolio Strategy')
+    popupMenu.pack(side=BOTTOM, padx=5,pady=5)
+
 def CreateForm(root):
     ents = makeform(root, fields)
+
     b2 = Button(root, text='Quit', command=root.quit)
     b2.pack(side=BOTTOM, padx=5, pady=5)
     b1 = Button(root, text='Calculate',command=(lambda e=ents: CalculatePortfolio(e)))
     b1.pack(side=BOTTOM, padx=5, pady=5)
+
+    CreateAllocationPopup()
 
 if __name__ == '__main__':
     root = Tk()
